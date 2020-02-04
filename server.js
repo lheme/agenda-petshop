c10fe7bf7c735d2820cd15c67425e5749ed1a196
Luiz Henrique Leme <lheme@outlook.com.br>
2020-02-03 21:40:32 -0300

    Adicionando um novo cliente com Mutation

--- server.js
+++ server.js
@@ -15,6 +15,13 @@ conexao.connect(erro => {
 const  resolvers = {
   Query: {
     status: () => "Servidor Rodando"
+  },
+  Mutation: {
+    adicionarCliente: (root, params) => ({
+      id: 1,
+      nome: params.nome,
+       cpf: params.cpf
+      })
   }
 }
 const servidor = new GraphQLServer({
